7dc9ef53b85b00467b127719a38152f77846c076
Joel <jronceros.byj@gmail.com>
2022-07-18 11:39:11 -0500

    Setup darkmode

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css"
 import { SessionProvider } from "next-auth/react"
 
+import { ThemeProvider } from "next-themes"
+
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   )
 }
