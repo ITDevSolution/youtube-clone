b3562996a36620e39d5eac9fc73489e18a21d819
Joel <jronceros.byj@gmail.com>
2022-07-22 12:02:03 -0500

    add domains a our project

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "bootcamp-joeloff.s3.sa-east-1.amazonaws.com",
+      "localhost",
+      "bootcamp-joeloff.s3.amazonaws.com",
+      "cloudflare-ipfs.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
